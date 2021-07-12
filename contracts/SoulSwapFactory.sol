72c809b94e235ad7433c72ff87ff41acf61fe5a7
Buns <bunsthedev@gmail.com>
2021-07-12 03:50:58 -0500

    update: factory

--- contracts/SoulSwapFactory.sol
+++ contracts/SoulSwapFactory.sol
@@ -26,35 +26,38 @@ contract SoulSwapFactory is ISoulSwapFactory {
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'SoulSwap: ZERO_ADDRESS');
         require(getPair[token0][token1] == address(0), 'SoulSwap: PAIR_EXISTS'); // single check is sufficient
+
         bytes memory bytecode = type(SoulSwapPair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        assembly {
-            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
-        }
+        
+        assembly { pair := create2(0, add(bytecode, 32), mload(bytecode), salt) }
         SoulSwapPair(pair).initialize(token0, token1);
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
         totalPairs++;
+        
         emit PairCreated(token0, token1, pair, totalPairs);
     }
 
     function setFeeTo(address _feeTo) external {
         require(msg.sender == feeToSetter, 'SoulSwap: FORBIDDEN');
         feeTo = _feeTo;
+        
         emit SetFeeTo(msg.sender, feeTo);
     }
 
     function setMigrator(address _migrator) external {
         require(msg.sender == feeToSetter, 'SoulSwap: FORBIDDEN');
         migrator = _migrator;
+        
         emit SetMigrator(msg.sender, migrator);
-
     }
 
     function setFeeToSetter(address _feeToSetter) external {
         require(msg.sender == feeToSetter, 'SoulSwap: FORBIDDEN');
         feeToSetter = _feeToSetter;
+        
         emit FeeToSetter(msg.sender, feeToSetter);
     }
 
