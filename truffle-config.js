cc3981fad53c4a7c8ea3f2294eefcc82db0d411c
Buns <bunsthedev@gmail.com>
2021-07-22 03:25:23 -0500

    v0.0.2

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,3 @@
-// require('babel-register');
-// require('babel-polyfill');
-// require('dotenv').config();
-// const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
-// const privateKeys = process.env.PRIVATE_KEYS || ""
-
 module.exports = {
   networks: {
       development: {
@@ -11,24 +5,6 @@ module.exports = {
         port: 7545,
         network_id: "*"
       },
-      // rinkeby: {
-      //   provider: function() {
-      //     return new HDWalletProvider(
-      //       privateKeys.split(','), // Array of account private keys
-      //       `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`// URL to an Ethereum Node
-      //     )
-      //   },
-      //   gas: 5000000,
-      //   gasPrice: 25000000000,
-      //   network_id: 4
-      // }
-    // },
-    // plugins: [
-    //   'truffle-plugin-verify'
-    // ],
-    // api_keys: {
-    //   etherscan: process.env.ETHERSCAN_API_KEY
-    // },
     compilers: {
       solc: {
         version: "0.5.16"
